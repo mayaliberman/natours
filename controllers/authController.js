306bcdce57e4af6b0e24c6bf96acb480762d1919
unknown <mayaliberman1@gmail.com>
2020-05-15 14:35:09 +0300

    first commit

---
+++ controllers/authController.js
@@ -0,0 +1,68 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+const signToken = id => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN
+  });
+};
+exports.signup = catchAsync(async (req, res, next) => {
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm
+  });
+
+  const token = signToken(newUser._id);
+
+  res.status(200).json({
+    status: 'sucess',
+    token,
+    data: { user: newUser }
+  });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  //Check if email and password exist
+  if (!email || !password) {
+    return next(new AppError('Please provide correct email and password', 400));
+  }
+  //check if the user exist && password is correct
+  const user = await User.findOne({ email }).select('+password');
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError('Incorrect email or password', 401));
+  }
+
+  //if everything is ok, send token to client
+  const token = signToken(user._id);
+
+  res.status(200).json({ status: 'sucess', token });
+});
+
+exports.protect = catchAsync(async (res, req, next) => {
+  console.log(req.headers.authorization);
+  //1. Get the token and check it's there
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+
+  if (!token) {
+    return next(
+      new AppError('You are not logged in! Please log in to get access.', 401)
+    );
+  }
+  
+  //2. verification  token
+  //3. Check if user exists
+  //4. check if user changed password after the token was issued
+  next();
+});
